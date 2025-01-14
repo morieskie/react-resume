61eacc625416263c98cb6dee77fe4f72818430e8
Derick Fynn <dcfynn@icloud.com>
2025-01-15 01:33:42 +0200

    feat(config): Added fallback values in case dotenv is not properly used by the environment

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,8 @@
+export const appUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.RESUME_APP_URL || "https://morieskie.github.io/react-resume"
+    : "http://localhost:3000/react-resume";
 export const apiUrl =
-  process.env.RESUME_APP_API_UR || "https://morieskie.github.io/react-resume";
+  process.env.NODE_ENV === "production"
+    ? process.env.RESUME_API_URL || "https://morieskie.github.io/react-resume"
+    : "http://localhost:3000/react-resume";
