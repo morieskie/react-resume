07a09dd09ba58bd4176f6d1267ee9fe94870e9de
Derick Fynn <dcfynn@icloud.com>
2025-01-15 16:29:14 +0200

    feat(redux): Added education, technologies & experience to userSliece
    feat(redux): Added additional selectors > user redux thunk

--- src/store/selectors/userSelectors.ts
+++ src/store/selectors/userSelectors.ts
@@ -1,5 +1,35 @@
+import { IExperience } from "../../components/home/IExperience";
 import { RootState } from "../index";
 
-export const userSelector = (state: RootState) => state.user.user;
+export const userSelector = (state: RootState) => state.user;
 
-// export const 
+export const userExperienceSelector = (state: RootState) =>
+  state.user.experience;
+
+export const userRolesSelector = (state: RootState) => [
+  ...new Set(state.user.experience.map((e) => e.role)),
+];
+
+export const userEducationSelector = (state: RootState) => state.user.education;
+export const userTechnologySelector = (state: RootState) => state.user.technologies;
+
+export const userExperienceByFilterSelector = (
+  state: RootState,
+  role: string,
+  from: number = 1977,
+  to: number = new Date().getFullYear()
+) => {
+  let roles = state.user.experience;
+
+  const experienceFilter = (e: IExperience) =>
+    new Date(e.from).getFullYear() >= from &&
+    new Date(e.to).getFullYear() <= to;
+
+  roles = roles.filter(experienceFilter);
+
+  if (role) {
+    const experienceRoleFilter = (e: any) => e.role.includes(role);
+    roles = roles.filter(experienceRoleFilter);
+  }
+  return roles;
+};
--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -1,25 +1,53 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IMe } from "../../components/home/IMe";
+import { IExperience } from "../../components/home/IExperience";
+import { ISocialLink } from "../../components/home/ISociatLink";
+import { IEducation } from "../../components/home/IEducation";
+import { ITechnology } from "../../components/pages/resume/ITechnology";
 
 export const userSlice = createSlice({
   name: "user",
   initialState: {
-    user: {
-      name: {
-        firstName: "Guest",
-        lastName: "User",
-      },
-      socialLinks: [] as any,
-    } as IMe,
+    name: {
+      firstName: "Guest",
+      lastName: "User",
+    },
+    bio: "",
+    mobileNumber: "",
+    email: "",
+    address: "",
+    socialLinks: [] as ISocialLink[],
+    experience: [] as IExperience[],
+    education: [] as IEducation[],
+    technologies: [] as ITechnology[],
   },
   reducers: {
-    setUser: (state: { user: IMe }, action: PayloadAction<IMe>) => ({
+    setUser: (state, action: PayloadAction<IMe>) => ({
       ...state,
-      user: { ...state.user, ...action.payload },
+      ...action.payload,
     }),
+    setUserExperience: (state, action: PayloadAction<IExperience[]>) => {
+      return {
+        ...state,
+        experience: action.payload,
+      };
+    },
+    setUserEducation: (state, action: PayloadAction<IEducation[]>) => {
+      return {
+        ...state,
+        education: action.payload,
+      };
+    },
+    setUserTechnologies: (state, action: PayloadAction<ITechnology[]>) => {
+      return {
+        ...state,
+        technologies: action.payload,
+      };
+    },
   },
 });
 
-export const { setUser } = userSlice.actions;
+export const { setUser, setUserExperience, setUserEducation, setUserTechnologies } =
+  userSlice.actions;
 
 export default userSlice.reducer;
